7993f5a70cc7c90d35ef5068015a1005f99e55f6
monikagrz <55914238+monikagrz@users.noreply.github.com>
2022-02-17 00:02:02 +0900

    Imported bootstrap, created new component Dictionary and built search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Dictionary</h1>
+      <div className="container">
+        <h1 className="App-header">Dictionary</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Monika</small>
+        </footer>
+      </div>
     </div>
   );
 }
