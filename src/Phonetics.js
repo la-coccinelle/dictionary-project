0b7f495279eb52d76bc73b3d7933dfc52a22e95d
monikagrz <55914238+monikagrz@users.noreply.github.com>
2022-03-12 00:23:43 +0900

    Edited audio button - added audio player

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
+  let audioElement = new Audio(props.phonetic.audio);
+
+  function handleClick() {
+    audioElement.play();
+  }
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        Listen
-      </a>
+      <button className="Audio-button" onClick={handleClick}>
+        Audio
+      </button>
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
